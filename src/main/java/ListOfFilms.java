22b5a5f8fcd0383ca4430c0127b75692197aac1c
Gabriel Winter <gabrielcwinter@gmail.com>
2022-03-31 13:54:45 -0300

    primeiro dia do desafio, printar de json com api imdb

---
+++ src/main/java/ListOfFilms.java
@@ -0,0 +1,26 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ListOfFilms {
+    public static void main(String[] args) throws URISyntaxException {
+
+        final String senha = "k_cp505m6c";
+
+        HttpRequest httpRequest = HttpRequest
+                .newBuilder()
+                .uri(new URI("https://imdb-api.com/en/API/Top250Movies/"+ senha))
+                .GET()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        client
+                .sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+    }
+}
